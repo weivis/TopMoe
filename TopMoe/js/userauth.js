23af79c59b88dca573744ffd03b4502b775566b6
ShenVi <yvban@outlook.com>
2019-03-26 17:18:22 +0800

    更新登录路由

---
+++ TopMoe/js/userauth.js
@@ -0,0 +1,23 @@
+var Userauth = {
+	
+	//判断用户是否登录 否则跳转登录页面
+	aloginfor : function(dom){
+			var user = localStorage.getItem("userid");
+			//if user == none
+			if(user){
+				alert('已登录')
+			}else{
+				mui.openWindow({
+					url: 'login.html',
+					id: 'login',
+					show: {
+						aniShow: true
+					},
+					waiting: {
+						autoShow: false
+					}
+				});
+			}
+	}
+	
+}
